8619d6b1185fbc8e370d839442d1f40325d182da
lenguyenwi <lenguyenwi@gmail.com>
2017-08-30 13:09:31 +0200

    first commit

---
+++ WebAPIDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Web.Http;
+using EmployeeDataAccess;
+using System.Collections.Generic;
+
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
